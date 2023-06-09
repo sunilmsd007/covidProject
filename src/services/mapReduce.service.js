dafdf028e1f81722c31c983f29654722ce65943c
sunilmsd007 <sunilmsd007@gmail.com>
2023-06-09 16:36:15 +0530

    push for jenkin

--- src/services/mapReduce.service.js
+++ src/services/mapReduce.service.js
@@ -78,7 +78,7 @@ export const mapReduceChek = async () => {
     }
 }
 
-//getmapReduce
+//getmapReduceCheck
 export const getmapReduce = async () => {
     const data = await Vaccination.aggregate(
         [
